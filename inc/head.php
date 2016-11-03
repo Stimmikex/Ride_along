fd8276979925eda056ec9d6dcf34c8da2e2d4b05
bjarkifannar <bjarkifannar@gmail.com>
2016-11-03 14:44:11 +0000

    Setup

---
+++ inc/head.php
@@ -0,0 +1,7 @@
+<?php
+	$websiteName = 'Ride Request';
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title><?php echo "$websiteName - $pageName - Under Construction!"; ?></title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	$pageName = 'Home';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require_once 'inc/head.php'; ?>
+	</head>
+	<body>
+		<h1 align="center">THIS WEBSITE IS UNDER CONSTRUCTION!</h1>
+	</body>
+</html>
