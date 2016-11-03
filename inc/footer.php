b61f79b38270a976f5ae266e8e996cc4535947de
Stimmikex <styrmir97@gmail.com>
2016-11-03 15:44:24 +0000

    Added header and footer
    nice!

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<div>
+		
+	</div>
+	<div>
+		<p class="copyright-text">&copy; 2016 | Styrmir Óli Þorsteinsson and Bjarki Fannar Snorrason</p>
+	</div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/sha512.js"></script>
+<script src="js/forms.js"></script>
+<?php $db = null; ?>
--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 		<?php require_once 'inc/head.php'; ?>
 	</head>
 	<body>
+		<?php require_once 'inc/header.php'; ?>
 		<h1 align="center">THIS WEBSITE IS UNDER CONSTRUCTION!</h1>
+		<?php require_once 'inc/footer.php'; ?>
 	</body>
 </html>
