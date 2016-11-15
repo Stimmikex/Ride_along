c7df930fad409645a5db03bc3518d37ebd8fef75
Stimmikex <styrmir97@gmail.com>
2016-11-15 13:16:50 +0000

    Header 1.0
    We are going to do more changes to the header but this is the start of
    the header

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,23 @@
 <header>
-	<nav>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href=""></a></li>
-			<li><a href=""></a></li>
+<!-- 	<nav>
+		<ul class="nav-menu">
+			<li><a href="index.php" class="nav-class">Home</a></li>
+			<li><a href="#">Hello</a></li>
+			<li><a href="#">World</a></li>
 		</ul>
+	</nav> -->
+	<nav class="navBar">
+	  <nav class="wrapper">
+	    <div class="logo"></div><!-- Logo -->
+	    <input type="checkbox" id="menu-toggle">
+	      <label for="menu-toggle" class="label-toggle"></label>
+	    </input>
+	    <ul>
+	      <a href="index.php"><li>Home</li></a>
+	      <a href="#"><li>Hello</li></a>
+	      <a href="#"><li>World</li></a>
+	    </ul>
+	  </nav>
 	</nav>
 </header>
 <div class="content-wrap">
