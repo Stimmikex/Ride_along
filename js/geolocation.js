944c407d0ceb8d04ab3a71f8fa9ae5e25dc8eda7
bjarkifannar <bjarkifannar@gmail.com>
2016-11-17 16:04:36 +0000

    Updated geolocation

--- js/geolocation.js
+++ js/geolocation.js
@@ -6,16 +6,10 @@
 function geolocation() {
 	if ("geolocation" in navigator) {
 		/* geolocation is available */
-		navigator.geolocation.watchPosition(function(position) {
-			displayLocation(position.coords.latitude, position.coords.longitude);
+		navigator.geolocation.getCurrentPosition(function(position) {
 		});
 	} else {
 		/* geolocation is NOT available */
 		console.error("geolocation is not available");
 	}
-}
-
-function displayLocation(lat, lon) {
-	pos = 'GeoLocation. lat: ' + lat + ', lon: ' + lon;
-	document.getElementById('geolocation').innerHTML = pos;
 }
