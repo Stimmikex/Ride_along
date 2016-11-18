34a62c46b3580c52a96ac66f8de44cf06635ff8d
bjarkifannar <bjarkifannar@gmail.com>
2016-11-18 10:55:39 +0000

    Updated the footer
    Added a warning message to the footer and fixed a bug with the
    background color on mobile

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer>
 	<div class="top-footer flex-container">
-		<div class="col-1-2 footer-padding">Left
+		<div class="col-1-2 footer-padding">
 			<div class="hero-circle">
 				<div class="hero-face">
 					<div id="hour" class="hero-hour"></div>
@@ -11,8 +11,10 @@
 			</div>
 		</div>
 		<div class="col-1-2 flex-container footer-padding">
-			<div class="col-1-2">Right-Left</div>
-			<div class="col-1-2">Right-Right</div>
+			<div class="col-1-2"></div>
+			<div class="col-1-2">
+				<p><b>WARNING!</b><br>You have to be 18 or older to use this site!<br>We are not responsible for anything our users do or say!</p>
+			</div>
 		</div>
 	</div>
 	<div class="bottom-footer">
