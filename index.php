e57eec4ee235bf9398a225c6351e1e436537248f
bjarkifannar <bjarkifannar@gmail.com>
2016-11-18 11:19:50 +0000

    Added links to the index
    If the user is logged in he sees links to offer a ride or request a
    ride.
    
    If the user is logged out he is prompted to log in.

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
 	</head>
 	<body>
 		<?php require_once 'inc/header.php'; ?>
+		<?php
+			if ($logged === 'in') {
+		?>
+		<ul>
+			<li><a href="#">Offer a ride</a></li>
+			<li><a href="#">Request a ride</a></li>
+		</ul>
+		<?php
+			} else {
+		?>
+		<p>Please <a href="login.php">log in</a> to offer or request a ride.</p>
+		<?php
+			}
+		?>
 		<?php require_once 'inc/footer.php'; ?>
 	</body>
 </html>
