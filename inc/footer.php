5b9c25e7f65d35ebc2ecc34c038889e676d057f1
Stimmikex <styrmir97@gmail.com>
2016-11-18 09:30:53 +0000

    footer_clock 1.0
    Have been having some trouble with one of the computers and im just
    putting this here so i can change computers.

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,15 @@
 </div>
 <footer>
 	<div class="top-footer flex-container">
-		<div class="col-1-2 footer-padding">Left</div>
+		<div class="col-1-2 footer-padding">Left
+			<div class="hero-circle">
+				<div class="hero-face">
+					<div id="hour" class="hero-hour"></div>
+					<div id="minute" class="hero-minute"></div>
+					<div id="second" class="hero-second"></div>
+				</div>
+			</div>
+		</div>
 		<div class="col-1-2 flex-container footer-padding">
 			<div class="col-1-2">Right-Left</div>
 			<div class="col-1-2">Right-Right</div>
@@ -14,4 +22,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="js/plugins.js"></script>
 <script src="js/geolocation.js"></script>
+<script src="js/footer_clock.js"></script>
 <?php $db = null; ?>
