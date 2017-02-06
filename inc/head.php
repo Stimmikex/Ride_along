db669de1793d7a70d09ae055b3fde43a59e46fd9
bjarkifannar <bjarkifannar@gmail.com>
2017-02-06 12:28:19 +0000

    Forcing HTTPS

--- inc/head.php
+++ inc/head.php
@@ -1,4 +1,9 @@
 <?php
+	if($_SERVER['HTTPS'] !== 'on') {
+		header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		exit();
+	}
+
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
