ebef85a5ecc440e976173c095249f1ec87fb504f
bjarkifannar <bjarkifannar@gmail.com>
2017-05-02 12:06:31 +0000

    Added the facebook login image

--- Laravel/resources/views/auth/login.blade.php
+++ Laravel/resources/views/auth/login.blade.php
@@ -5,11 +5,10 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
 				<div class="panel-body">
 					<div class="form-group">
 						<div class="col-md-8 col-md-offset-4">
-							<a href="redirect">FB Login</a>
+							<a href="redirect"><img src="{{ config('paths.img') }}fb_login.png"></a>
 						</div>
 					</div>
 				</div>
