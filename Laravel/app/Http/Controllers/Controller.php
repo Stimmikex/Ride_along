55f46703c53ab595a815fdb3035f232eb2289b04
bjarkifannar <bjarkifannar@gmail.com>
2017-05-04 10:37:06 +0000

    Added the notification views

--- Laravel/app/Http/Controllers/Controller.php
+++ Laravel/app/Http/Controllers/Controller.php
@@ -11,18 +11,24 @@ use Auth;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function logincheck() {
-    	if (Auth::guest()) {
-    		return redirect()->to('/home');
+	public function logincheck() {
+		if (Auth::guest()) {
+			return redirect()->to('/home');
 		}
 		else {
 			return true;
 		}
-    }
-    public function location_check() {
-    	$dp_get_schedule = DB::table('planner')->select('id', 'plan_name')->where('user_id', $user_id['id'])->get();
-    	$location = DB::table('')->select('name')->where('')->get();
-    }
+	}
+
+	public function getUserID() {
+		if($this->logincheck())
+			return Auth::getUser()['attributes']['id'];
+	}
+
+	public function location_check() {
+		$dp_get_schedule = DB::table('planner')->select('id', 'plan_name')->where('user_id', $user_id['id'])->get();
+		$location = DB::table('')->select('name')->where('')->get();
+	}
 }
